e45349b8194dbdb24f05019363c40a6f1f1745b8
PadmapriyaNagarajan <144335238+PadmapriyaNagarajan@users.noreply.github.com>
2023-11-03 18:15:00 +0530

    i want to add

---
+++ datatables/src/main/java/datatables/datatables/App.java
@@ -0,0 +1,13 @@
+package datatables.datatables;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
